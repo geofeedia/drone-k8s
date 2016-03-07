b360d98fb605b76550eec329071dc9fcc93ddec3
Austen Lacy <austenlacy@gmail.com>
2016-03-07 10:41:11 -0500

    removed docker login as we assume we are already running inside k8s

--- Makefile
+++ Makefile
@@ -5,9 +5,9 @@ VERSION=1.0.0
 all: build tags
 
 build:
-    docker build -f Dockerfile.build -t quay.io/geofeedia/${PROJECT}-build:${VERSION} .
-    docker run --rm quay.io/geofeedia/${PROJECT}-build:${VERSION} | docker build -t quay.io/geofeedia/${PROJECT}:${VERSION} -
-    docker rmi -f quay.io/geofeedia/${PROJECT}-build:${VERSION}
+	docker build -f Dockerfile.build -t quay.io/geofeedia/${PROJECT}-build:${VERSION} .
+	docker run --rm quay.io/geofeedia/${PROJECT}-build:${VERSION} | docker build -t quay.io/geofeedia/${PROJECT}:${VERSION} -
+	docker rmi -f quay.io/geofeedia/${PROJECT}-build:${VERSION}
 
 tags:
-    docker tag quay.io/geofeedia/${PROJECT}:${VERSION} quay.io/geofeedia/${PROJECT}:${MAJOR_VERSION}
+	docker tag quay.io/geofeedia/${PROJECT}:${VERSION} quay.io/geofeedia/${PROJECT}:${MAJOR_VERSION}
--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import(
     "fmt"
-    "time"
     "log"
     "os"
     "os/exec"
-    "io/ioutil"
     "strings"
     
     "github.com/drone/drone-go/drone"
@@ -15,11 +13,7 @@ import(
 
 type PluginParams struct {
     ReplicationController string `json:"replication_controller"`
-    Registry              string `json:"registry"`
-    Image                 string `json:"image"`
-    Username              string `json:"username"`
-    Email                 string `json:"email"`
-    Password              string `json:"password"`
+    DockerImage              string `json:"docker_image"`
     UpdatePeriod          string `json:"update_period"`
     Timeout               string `json:"timeout"`
 }
@@ -34,7 +28,7 @@ func main() {
         sys          = new(drone.System)
         pluginParams = new(PluginParams)
     )
-
+    
     plugin.Param("workspace", workspace)
     plugin.Param("build", build)
     plugin.Param("repo", repo)
@@ -46,70 +40,17 @@ func main() {
         log.Fatal("No replication controller name provided. Unable to continue.")
     }
     
-    if len(pluginParams.Image) == 0 {
+    if len(pluginParams.DockerImage) == 0 {
         log.Fatal("No image name provided. Unable to continue.")
     }
     
-    if len(pluginParams.Registry) == 0 {
-        // default docker hub registry if none provided
-        pluginParams.Registry = "https://index.docker.io/v1/"
-    }
-    
-    if len(pluginParams.UpdatePeriod) == 0 {
-        // default for kubectl rolling-update `--update-period` is 60 seconds
-        pluginParams.UpdatePeriod = "1m0s"
-    }
-    
-    if len(pluginParams.Timeout) == 0 {
-        // default for kubectl rolling-update `--timeout` is 5 minutes
-        pluginParams.Timeout = "5m0s"
-    }
-    
-    if len(pluginParams.Email) == 0 {
-        pluginParams.Email = "mail@mail.com"
-    }
-    
-    // ping Docker until available
-    for i := 0; i < 3; i++ {
-        cmd := exec.Command("/usr/bin/docker", "info")
-        cmd.Stdout = ioutil.Discard
-        cmd.Stderr = ioutil.Discard
-        
-        trace(cmd)
-        err := cmd.Run()
-        if err == nil {
-            break
-        }
-        time.Sleep(time.Second * 5)
-    }
-    
-    if len(pluginParams.Username) != 0 {
-        cmd := exec.Command("/usr/bin/docker", "login",
-             "-u", pluginParams.Username,
-             "-p", pluginParams.Password,
-             "-e", pluginParams.Email,
-             pluginParams.Registry)
-         
-        cmd.Stdout = os.Stdout
-        cmd.Stderr = os.Stderr
-        trace(cmd)
-        err := cmd.Run()
-        if err != nil {
-            fmt.Printf("%s", err)
-            log.Fatal("Unable to login to provided Docker registry with given credentials.")
-        }
-    } else {
-        fmt.Println("No username provided so proceeding to use anonymous publishing.")
-    }
-    
-    // kubectl rolling-update --update-period=5s bouncer-e1b-v1.0.0 --image=gcr.io/geofeedia-qa1/service-enterprise-permissions:1.0.0
+    // kubectl rolling-update --update-period=5s YOUR-RC-e1b-v1.0.0 --image=registry/org/repo:1.0.0
     cmd := exec.Command(
         "/usr/bin/kubectl",
-        "rolling-update", pluginParams.Image,
+        "rolling-update", pluginParams.ReplicationController,
         "--update-period", pluginParams.UpdatePeriod,
         "--timeout", pluginParams.Timeout,
-        pluginParams.ReplicationController,
-        "--image", pluginParams.Image)
+        "--image", pluginParams.DockerImage)
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
     
