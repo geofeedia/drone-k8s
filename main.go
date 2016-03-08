cbf341aa31b35d26cf4a6e006d1c732294373aa1
Austen Lacy <austenlacy@gmail.com>
2016-03-08 16:43:07 -0500

    it works...

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import(
     "os"
     "os/exec"
     "strings"
+    "errors"
     
     "github.com/drone/drone-go/drone"
     "github.com/drone/drone-go/plugin"
@@ -13,7 +14,14 @@ import(
 
 type PluginParams struct {
     ReplicationController string `json:"replication_controller"`
-    DockerImage              string `json:"docker_image"`
+    DockerImage           string `json:"docker_image"`
+    Namespace             string `json:"namespace"`
+    K8sServiceHost        string `json:"k8s_service_host"`
+    K8sServicePort        string `json:"k8s_service_port"`
+    Protocol              string `json:"protocol"`
+    PathToCertAuth        string `json:"path_to_cert_authority"`
+    PathToClientKey       string `json:"path_to_client_key"`
+    PathToClientCert      string `json:"path_to_client_cert"`
     UpdatePeriod          string `json:"update_period"`
     Timeout               string `json:"timeout"`
 }
@@ -27,6 +35,8 @@ func main() {
         build        = new(drone.Build)
         sys          = new(drone.System)
         pluginParams = new(PluginParams)
+        cmd          = new(exec.Cmd)
+        err          = errors.New("err")
     )
     
     plugin.Param("workspace", workspace)
@@ -44,18 +54,46 @@ func main() {
         log.Fatal("No image name provided. Unable to continue.")
     }
     
-    // kubectl rolling-update --update-period=5s YOUR-RC-e1b-v1.0.0 --image=registry/org/repo:1.0.0
-    cmd := exec.Command(
+    if len(pluginParams.Namespace) == 0 {
+        pluginParams.Namespace = "default"
+    }
+    
+    if len(pluginParams.UpdatePeriod) == 0 {
+        pluginParams.UpdatePeriod = "1m0s"
+    }
+    
+    if len(pluginParams.Timeout) == 0 {
+        pluginParams.Timeout = "5m0s"
+    }
+    
+    if len(pluginParams.Protocol) == 0 {
+        pluginParams.Protocol = "https://"
+    }
+    
+    if len(pluginParams.K8sServiceHost) == 0 {
+        pluginParams.K8sServiceHost = "10.100.0.1"
+    }
+    
+    if len(pluginParams.K8sServicePort) == 0 {
+        pluginParams.K8sServicePort = "443"
+    }
+
+    cmd = exec.Command(
         "/usr/bin/kubectl",
         "rolling-update", pluginParams.ReplicationController,
+        "-s", pluginParams.Protocol + pluginParams.K8sServiceHost + ":" + pluginParams.K8sServicePort,
+        "--namespace", pluginParams.Namespace,
+        "--certificate-authority", pluginParams.PathToCertAuth,
+        "--client-key", pluginParams.PathToClientKey,
+        "--client-certificate", pluginParams.PathToClientCert,
         "--update-period", pluginParams.UpdatePeriod,
         "--timeout", pluginParams.Timeout,
         "--image", pluginParams.DockerImage)
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
-    
+
     trace(cmd)
-    err := cmd.Run()
+    err = cmd.Run()
     if err != nil {
         fmt.Printf("%s", err)
         log.Fatal("Unable to complete kubernetes rolling-update.")
@@ -63,7 +101,7 @@ func main() {
 }
 
 // Trace writes each command to standard error (preceded by a ‘$ ’) before it
-// is executed. Used for debugging your build.
+// is executed. Used for debugging the build.
 func trace(cmd *exec.Cmd) {
     fmt.Println("$", strings.Join(cmd.Args, " "))
 }
